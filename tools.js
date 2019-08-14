1cf1877167a638811faaefcfe210962149555141
Alex Ssanya <alexssanya@gmail.com>
2019-08-14 19:23:20 +0300

    refactor fetch function

--- tools.js
+++ tools.js
@@ -1,16 +1,16 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 const siteFetcher = async (site_url) => {
-    await axios.get(site_url)
+    const result = await axios.get(site_url)
     .then(res => {
-        return cheerio.load(res.data);
+        return res.data;
     })
     .catch(error => {
         console.log('error occured ', error)
         return 'error occured';
     })
     
+    return result;
 }
 
 module.exports = {
