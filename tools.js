ebeb690346231ed655d7b40e5a36430817e84065
Alex Ssanya <alexssanya@gmail.com>
2019-08-14 18:22:05 +0300

    add error handling to axios request

--- tools.js
+++ tools.js
@@ -2,9 +2,15 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const siteFetcher = async (site_url) => {
-    const result = await axios.get(site_url);
-    console.log(`Results => ${result.data}`);
-    return cheerio.load(result.data);
+    await axios.get(site_url)
+    .then(res => {
+        return cheerio.load(res.data);
+    })
+    .catch(error => {
+        console.log('error occured ', error)
+        return 'error occured';
+    })
+    
 }
 
 module.exports = {
